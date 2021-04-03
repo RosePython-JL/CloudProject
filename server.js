f58e7aa51e05cd37874d5fa67279de71aacb305e
minglaulomo <45078945+minglaulomo@users.noreply.github.com>
2021-04-03 13:53:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(express.json());
 app.use("/api/v1/posts", postRoutes);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.use(express.static(path.join(__dirname, "/client/build")));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     });
 }
 else{
-    app.get('/', (req, res) => {
+    app.get("*", (req, res) => {
         res.send("Api running");
     });
 }
